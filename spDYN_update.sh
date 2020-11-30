0d90234496311fa87b68e7bb31f0d3790a3058cc
Joel Gerhold <joel@gmx.de>
2020-11-30 18:51:15 +0100

    update spDYN_update.sh

--- spDYN_update.sh
+++ spDYN_update.sh
@@ -57,30 +57,29 @@ fi
 if [ "${ip}" = "${old_ip}" ]
 then
   echo "${DATE}: no IP change (${ip})" | tee -a "${log_file}"
-  exit 0
-fi
-
-if [ -z "${ip6}" ]
-then
-  echo "${DATE}: error retrieving IP6" | tee -a "${log_file}"
-  exit 1
-fi
-if [ "${ip6}" = "${old_ip6}" ]
-then
-  echo "${DATE}: no IP6 change (${ip6})" | tee -a "${log_file}"
-  exit 0
-fi
-
-
+else
 echo "${DATE}: detected new IP: ${old_ip} -> ${ip}" | tee -a "${log_file}"
 echo "${ip}" > "${ip_file}"
 echo "${DATE}: wrote new IP to ${ip_file}" | tee -a "${log_file}"
 response=$(curl -su "${HOST_NAME}:${UPDATE_TOKEN}" "${UPDATE_URL}?hostname=${HOST_NAME}&myip=${ip}")
 echo "${DATE}: response: ${response}" | tee -a "${log_file}"
+fi
 
-
+if [ -z "${ip6}" ]
+then
+  echo "${DATE}: error retrieving IP6" | tee -a "${log_file}"
+fi
+if [ "${ip6}" = "${old_ip6}" ]
+then
+  echo "${DATE}: no IP6 change (${ip6})" | tee -a "${log_file}"
+  exit 0
+else
 echo "${DATE}: detected new IP6: ${old_ip6} -> ${ip6}" | tee -a "${log_file}"
 echo "${ip6}" > "${ip6_file}"
 echo "${DATE}: wrote new IP to ${ip6_file}" | tee -a "${log_file}"
 response=$(curl -su "${HOST_NAME}:${UPDATE6_TOKEN}" "${UPDATE_URL}?hostname=${HOST_NAME}&myip=${ip6}")
 echo "${DATE}: response: ${response}" | tee -a "${log_file}"
+fi
+
+
+
